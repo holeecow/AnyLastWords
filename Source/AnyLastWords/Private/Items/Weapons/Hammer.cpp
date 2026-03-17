1a7829410b456baa65aec4d04944971a1ae1a911
zexuan <jasonteo032@gmail.com>
2026-03-18 05:12:26 +0800

    Setup base Item and Melee classes
    - Setup Item class to trigger detect overlap events so that they can be picked up
    - Setup Hammer class to inherit from the base Item class
    - Import weapon assets from Fab

---
+++ Source/AnyLastWords/Private/Items/Weapons/Hammer.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Weapons/Hammer.h"
+
+void AHammer::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void AHammer::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
---
+++ Source/AnyLastWords/Public/Items/Weapons/Hammer.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Hammer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANYLASTWORDS_API AHammer : public AItem
+{
+	GENERATED_BODY()
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+};
