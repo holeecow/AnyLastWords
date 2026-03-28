92f0f7a05f4a73362de82ee97f2632dbf01dc55b
zexuan <jasonteo032@gmail.com>
2026-03-28 18:05:01 +0800

    fix: player not being able to attack after getting hit

--- Source/AnyLastWords/Public/Characters/CharacterTypes.h
+++ Source/AnyLastWords/Public/Characters/CharacterTypes.h
@@ -13,4 +13,5 @@ enum class EActionState : uint8
 {
 	EAS_Unoccupied UMETA(DisplayName = "Unoccupied"),
 	EAS_Attacking UMETA(DisplayName = "Attacking"),
+	EAS_GettingHit UMETA(DisplayName = "Getting Hit"),
 };
